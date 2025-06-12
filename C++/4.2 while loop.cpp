aa7f3174e1526d4402e84014361b6cf0e480f24d
Ankur <ankur2002chauhan@gmail.com>
2025-06-12 18:29:44 +0530

    C++ work

---
+++ C++/4.2 while loop.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int num = 40;
+    while(num<=10){
+        cout<<num<<endl;
+        num = num+1;
+    }
+    // int x =50;
+    // do{
+    //     cout<<x<<endl;
+    //     x = x + 10;
+    // } while(x<=40);
+    
+
+    return 0;
+}
---
+++ C++/5 arrays.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num[5];
+    for(int i = 0; i < 5; i++){
+        cin >> num[i];
+        cout << num[i] << endl;
+    }
+
+
+    return 0;
+}
