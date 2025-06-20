a9fbed6a872678bf6848a6e84fe17e80b6a4072a
Ankur <ankur2002chauhan@gmail.com>
2025-06-20 22:47:43 +0530

    c++

--- C++/Standalone/Patterns/12.  Character Pattern.cpp
+++ C++/Standalone/Patterns/12.  Character Pattern.cpp
@@ -14,7 +14,10 @@ public:
               
             cout<<ch;
             }
-          
+            /* As soon as the letters for each 
+            iteration are printed, we move to the
+            next row and give a line break otherwise
+            all letters would get printed in 1 line.*/
             cout<<endl;
         }
     }
