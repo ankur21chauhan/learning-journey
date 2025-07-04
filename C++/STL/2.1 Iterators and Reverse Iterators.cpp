36a35bff958250034351c402e045bb56911ce576
Ankur <ankur2002chauhan@gmail.com>
2025-07-04 19:02:21 +0530

    File Updated

--- C++/STL/2.1 Iterators and Reverse Iterators.cpp
+++ C++/STL/2.1 Iterators and Reverse Iterators.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 void explainVectors(){
 
+    //initializing array
     vector<int> var1 = {10,20,30,40,50};
 
+    //printing the entire array using iterator
     vector<int>::iterator beginItr = var1.begin();
     vector<int>::iterator endItr = var1.end();
     
@@ -11,6 +13,15 @@ void explainVectors(){
         cout<<*i<<" ";
     }
     cout<<endl;
+
+    auto varuabl1 = var1.rbegin();
+    auto varuabl2 = var1.rend();
+
+    for(auto i = varuabl1; i < varuabl2; i++){
+        cout<<*i<<" ";
+    }
+
+
 }
 
 int main(){
