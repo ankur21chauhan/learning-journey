2f948dccb1f548cf3df7ebffabbeddca3ebfa343
Ankur <ankur2002chauhan@gmail.com>
2025-08-15 16:07:03 +0530

    Code updated

--- C++/Arrays/Problem 3.cpp
+++ C++/Arrays/Problem 3.cpp
@@ -3,13 +3,11 @@ using namespace std;
 class A{
     private:
         void reverse(int arr[], int n){
-            int temp[n];
-            for(int i = 0; i <= n-1; i++){
-                temp[n-1-i] = arr[i]; 
-            }
-
-            for(int i = 0; i <= n-1;i++){
-                arr[i] = temp[i];
+            int left = 0, right = n-1;
+            while(left < right){
+                swap(arr[left], arr[right]);
+                left++;
+                right--;
             }
         }
 
