50d34b1e06a834b525c59af6c3a7758ad4ad19d2
ankur21chauhan <ankur2002chauhan@gmail.com>
2026-04-10 01:36:34 +0530

    Static Keyword

---
+++ C++/OOPS/Statis Keyword (Static Methods).cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Utility{
+    public:
+        static int addition(a,b){
+            return (a + b);
+        } 
+};
+
+int main(){
+    cout<<(Utility::addition(2,3));
+}
